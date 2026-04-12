9aa6d49a70ed736d6dafd05a1da9670da4f58115
TonyBlu <zehao100@qq.com>
2026-04-13 01:10:02 +0800

    fix: remove tracked local dev files and update sync-upstream workflow

--- test_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export http_proxy=http://host.docker.internal:7897
-export https_proxy=http://host.docker.internal:7897
-wget -q -O - \
-  --post-data='{"project":"unique-case-862g6"}' \
-  --header='Authorization: Bearer ya29.a0Aa7MYioowZjSHnTg-fg1Od_EfjwZiDC9jsuv5t44s4E1DfoQeztGXyLh1amOLzLzgakdZD2dtIwIRTK0YZHz7VmWwLcAMKHmd4NmjKoS9F0M3pw9HWNFuWpzI1tLcNz836g8CqgpEIYM9gLnKRHBOfwIGoy1F6t-nIXEWf7dDjke21JdN0lvvT6pJjTNwx9fx6Aarc-7dPDpaCgYKAcMSARUSFQHGX2MidGzZVttH0T5NpunRjf9kcg0211' \
-  --header='Content-Type: application/json' \
-  https://cloudcode-pa.googleapis.com/v1internal:fetchAvailableModels
